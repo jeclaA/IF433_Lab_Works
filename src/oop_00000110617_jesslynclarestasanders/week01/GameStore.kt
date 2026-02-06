9e8a5c7dd34fa6bb44cdcd15c55e9476887d7518
Jesslyn Claresta Sanders <jesslynclarestasanders@Jesslyns-MacBook-Pro.local>
2026-02-06 11:35:19 +0700

    week01: (tugas 2) implement receipt printing

--- src/oop_00000110617_jesslynclarestasanders/week01/GameStore.kt
+++ src/oop_00000110617_jesslynclarestasanders/week01/GameStore.kt
@@ -4,5 +4,8 @@ fun main(){
     val gameTitle: String = "Overcooked"
     val price: Int = 500000
     val hargaAkhir = calculateDiscount(price)
+    printReceipt(title = gameTitle, ogPrice = price, finalPrice = hargaAkhir))
+
 }
 fun calculateDiscount(price: Int) = if(price > 500000) price - (price*20/100) else price - (price*10/100)
+fun printReceipt(title: String, ogPrice: Int, finalPrice: Int) = println("Judul: $title, Harga Asli: $ogPrice, Harga Akhir: $finalPrice")
