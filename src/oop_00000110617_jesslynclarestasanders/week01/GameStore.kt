708b3515637597180517574bab8ab9fda43f0721
Jesslyn Claresta Sanders <jesslynclarestasanders@Jesslyns-MacBook-Pro.local>
2026-02-06 11:26:20 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000110617_jesslynclarestasanders/week01/GameStore.kt
+++ src/oop_00000110617_jesslynclarestasanders/week01/GameStore.kt
@@ -3,4 +3,6 @@ package oop_00000110617_jesslynclarestasanders.week01
 fun main(){
     val gameTitle: String = "Overcooked"
     val price: Int = 500000
+    val hargaAkhir = calculateDiscount(price)
 }
+fun calculateDiscount(price: Int) = if(price > 500000) price - (price*20/100) else price - (price*10/100)
