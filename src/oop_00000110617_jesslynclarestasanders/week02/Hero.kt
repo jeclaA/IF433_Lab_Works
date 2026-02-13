ab15e5acf8f664389533b256926ce5e927886eb5
Jesslyn Claresta Sanders <jesslynclarestasanders@Jesslyns-MacBook-Pro.local>
2026-02-13 11:51:18 +0700

    week02: (task 2) implement the game

--- src/oop_00000110617_jesslynclarestasanders/week02/Hero.kt
+++ src/oop_00000110617_jesslynclarestasanders/week02/Hero.kt
@@ -1,7 +1,5 @@
 package oop_00000110617_jesslynclarestasanders.week02
-
-import oop_00000110617_jesslynclarestasanders.week02.Student
-
+import java.util.Scanner
 class Hero (
     val name: String,
     var hp: Int = 100,
@@ -27,4 +25,55 @@ class Hero (
             return false
         }
     }
+}
+fun main() {
+    val scanner = Scanner(System.`in`)
+
+    print("Masukkan nama Hero: ")
+    val heroName = scanner.nextLine()
+
+    print("Masukkan Base Damage Hero: ")
+    val damage = scanner.nextInt()
+
+    val hero = Hero(heroName, damage)
+    var enemyHp = 100
+
+    while (hero.isAlive() && enemyHp > 0) {
+        println("\n=== MENU ===")
+        println("1. Serang")
+        println("2. Kabur")
+        print("Pilihan: ")
+        val choice = scanner.nextInt()
+
+        if (choice == 1) {
+            hero.attack("Musuh")
+            enemyHp -= hero.baseDamage
+            if (enemyHp < 0) enemyHp = 0
+            println("Sisa HP Musuh: $enemyHp")
+
+            if (enemyHp > 0) {
+                val enemyDamage = (10..20).random()
+                println("Musuh membalas dengan $enemyDamage damage!")
+                hero.takeDamage(enemyDamage)
+
+                println("Sisa HP ${hero.name}: ${hero.hp}")
+            }
+        } else if (choice == 2) {
+            println("${hero.name} kabur dari pertarungan!")
+            break
+        } else {
+            println("Pilihan tidak valid!")
+        }
+    }
+
+    println("\n=== HASIL ===")
+
+    if (hero.isAlive() && enemyHp <= 0) {
+        println("${hero.name} MENANG! YAYYYRR")
+    } else if (!hero.isAlive()) {
+        println("${hero.name} kalah... Musuh menang")
+    } else {
+        println("Seri! Tidak ada yang Menang.")
+    }
+
 }
