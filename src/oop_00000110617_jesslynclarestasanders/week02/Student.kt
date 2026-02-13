75c35049f10d78c13813878ba1db5233b27b3eb7
Jesslyn Claresta Sanders <jesslynclarestasanders@Jesslyns-MacBook-Pro.local>
2026-02-13 10:51:30 +0700

    week02: challenge refactor primary constructor using default args

--- src/oop_00000110617_jesslynclarestasanders/week02/Student.kt
+++ src/oop_00000110617_jesslynclarestasanders/week02/Student.kt
@@ -3,7 +3,8 @@ package oop_00000110617_jesslynclarestasanders.week02
 class Student (
     val name: String,
     val nim: String,
-    var major: String
+    var major: String,
+    var gpa: Double = 0.0
 ) {
     init {
         if (nim.length != 5) {
