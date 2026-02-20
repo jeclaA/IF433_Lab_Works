7ec6d753e82d20cf68400c150039a99598885c7d
Jesslyn Claresta Sanders <jesslynclarestasanders@Jesslyns-MacBook-Pro.local>
2026-02-20 11:37:26 +0700

    week03: (task 2) implement Player testing in main

---
+++ src/oop_00000110617_jesslynclarestasanders/week03/MainPlayer.kt
@@ -0,0 +1,10 @@
+package oop_00000110617_jesslynclarestasanders.week03
+
+fun main() {
+    val player = Player("Jess")
+    // simulasi error:
+    //player.xp = 1
+
+    player.addXp(50)
+    player.addXp(60)
+}
