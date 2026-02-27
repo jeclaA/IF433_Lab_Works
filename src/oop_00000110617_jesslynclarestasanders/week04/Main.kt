a8fd54837a247c5a33c18c140bd05b2d6c8f2354
Jesslyn Claresta Sanders <jesslynclarestasanders@Jesslyns-MacBook-Pro.local>
2026-02-27 10:22:54 +0700

    week04: (task 1) test ElectricCar in main

--- src/oop_00000110617_jesslynclarestasanders/week04/Main.kt
+++ src/oop_00000110617_jesslynclarestasanders/week04/Main.kt
@@ -11,4 +11,10 @@ fun main() {
     myCar.openTrunk()
     myCar.honk()
     myCar.accelerate()
+
+    println("\n--- Testing Electric Car ---")
+    val myEC = ElectricCar("Tesla", 6, 85)
+    myEC.openTrunk()
+    myEC.honk()
+    myEC.accelerate()
 }
