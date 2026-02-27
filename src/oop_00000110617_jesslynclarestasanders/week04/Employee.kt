c4bc3e79f14d115f079dd9c07c15c95b64a96890
Jesslyn Claresta Sanders <jesslynclarestasanders@Jesslyns-MacBook-Pro.local>
2026-02-27 10:25:01 +0700

    week04: (task 2) create parent class Employee

---
+++ src/oop_00000110617_jesslynclarestasanders/week04/Employee.kt
@@ -0,0 +1,15 @@
+package oop_00000110617_jesslynclarestasanders.week04
+
+open class Employee(
+    val name: String,
+    val baseSalary: Int
+) {
+
+    open fun work() {
+        println("$name sedang bekerja.")
+    }
+
+    open fun calculateBonus(): Int {
+        return (baseSalary * 0.1).toInt()
+    }
+}
