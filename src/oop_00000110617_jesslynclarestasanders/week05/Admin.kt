0851a2c7d083f3569e9363686baf524d9b3a522c
Jesslyn Claresta Sanders <jesslynclarestasanders@Jesslyns-MacBook-Pro.local>
2026-03-06 10:34:35 +0700

    week05: create Admin class inheriting Pegawai

---
+++ src/oop_00000110617_jesslynclarestasanders/week05/Admin.kt
@@ -0,0 +1,10 @@
+package oop_00000110617_jesslynclarestasanders.week05
+
+class Admin(nama: String) : Pegawai(nama) {
+    override fun bekerja() {
+        println("[$nama] sedang duduk di depan komputer melayani administrasi.")
+    }
+    fun doAdminWork(){
+        println("[$nama] sedang merekap data absensi mahasiswa.")
+    }
+}
