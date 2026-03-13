1ee405d2b9bc41fa7e1ee136d838505054e27414
Jesslyn Claresta Sanders <jesslynclarestasanders@Jesslyns-MacBook-Pro.local>
2026-03-13 11:15:08 +0700

    week06: (task) instantiate smart devices in main

---
+++ src/oop_00000110617_jesslynclarestasanders/week06/MainTask.kt
@@ -0,0 +1,15 @@
+package oop_00000110617_jesslynclarestasanders.week06
+
+fun main() {
+
+    val hub = SmartHomeHub()
+
+    val lamp = SmartLamp("L1", "Ruang Tamu")
+    val speaker = SmartSpeaker("S1", "Google Nest Dapur")
+    val cctv = SmartCCTV("C1", "Ezviz Garasi")
+
+    hub.addDevice(lamp)
+    hub.addDevice(speaker)
+    hub.addDevice(cctv)
+
+}
