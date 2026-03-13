64460239cb8c6b142ef1626be7a13da155d80506
Jesslyn Claresta Sanders <jesslynclarestasanders@Jesslyns-MacBook-Pro.local>
2026-03-13 11:02:35 +0700

    week06: (task) create SmartHomeHub logic

---
+++ src/oop_00000110617_jesslynclarestasanders/week06/SmartHomeHub.kt
@@ -0,0 +1,19 @@
+package oop_00000110617_jesslynclarestasanders.week06
+
+class SmartHomeHub {
+
+    val devices = mutableListOf<SmartDevice>()
+
+    fun addDevice(device: SmartDevice) {
+        devices.add(device)
+    }
+
+    fun turnOffAllSwitches() {
+        for (device in devices) {
+            if (device is Switchable) {
+                device.turnOff()
+            }
+        }
+    }
+}
+
