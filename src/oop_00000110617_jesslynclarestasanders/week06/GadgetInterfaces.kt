b6ea8db512cff4111332af3aa83b047e35dba22a
Jesslyn Claresta Sanders <jesslynclarestasanders@Jesslyns-MacBook-Pro.local>
2026-03-13 10:35:12 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_00000110617_jesslynclarestasanders/week06/GadgetInterfaces.kt
@@ -0,0 +1,8 @@
+package oop_00000110617_jesslynclarestasanders.week06
+ interface Camera{
+     fun turnOn() { println("Lensa kamera terbuka dan sensor aktif") }
+}
+
+interface Phone {
+    fun turnOn() { println("Sinyal seluler mencari jaringan.") }
+}
