657a286d97b7d2e57de5dc969ec602e58e976042
Jesslyn Claresta Sanders <jesslynclarestasanders@Jesslyns-MacBook-Pro.local>
2026-03-27 11:18:01 +0700

    week07: (task) simulate rarity and factory instantiation

--- src/oop_00000110617_jesslynclarestasanders/week07/MainTask.kt
+++ src/oop_00000110617_jesslynclarestasanders/week07/MainTask.kt
@@ -4,4 +4,14 @@ fun main() {
     println("=== TEST GAME MANAGER (SINGLETON) ===")
     GameManager.startGame()
     GameManager.startGame()
+
+    println("\n=== TEST FACTORY & ENUM ===")
+    println("Drop chance Legendary Item: ${ItemRarity.LEGENDARY.dropChance}")
+
+    val starterWeapon = Weapon.forgeStarterSword()
+    println("\n[Senjata awal]")
+    println("Nama: ${starterWeapon.item.name}")
+    println("Damage: ${starterWeapon.item.damage}")
+    println("Rarity: ${starterWeapon.item.rarity}")
+    println("Durability: ${starterWeapon.durability}")
 }
