a0dff179340c4b44bc903c1aa05162d1285d0900
Jesslyn Claresta Sanders <jesslynclarestasanders@Jesslyns-MacBook-Pro.local>
2026-03-27 11:08:35 +0700

    week07: (task) implement exhaustive processEvent

---
+++ src/oop_00000110617_jesslynclarestasanders/week07/GameExecutor.kt
@@ -0,0 +1,20 @@
+package oop_00000110617_jesslynclarestasanders.week07
+
+fun processEvent(event: BattleState) {
+    val message = when (event) {
+        is BattleState.MonsterEncounter -> {
+            "[Monster muncul rGrggrH]: ${event.monsterName}! Bersiaplah;)!"
+        }
+        is BattleState.LootDropped -> {
+            val (name, _, rarity) = event.item
+            "Yayy kamu mendapatkan item: $name [Rarity: $rarity]"
+        }
+        is BattleState.GameOver -> {
+            "[OH No.. Game Over!] Alasan: ${event.reason}"
+        }
+        BattleState.SafeZone -> {
+            "[Tenang..] Kamu lagi di Safe Zone! Sementara ini masih aman bos"
+        }
+    }
+    println(message)
+}
