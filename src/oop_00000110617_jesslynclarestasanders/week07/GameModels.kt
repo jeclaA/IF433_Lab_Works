2edaec1f17958b56edb098745c2863127220cffa
Jesslyn Claresta Sanders <jesslynclarestasanders@Jesslyns-MacBook-Pro.local>
2026-03-27 10:57:06 +0700

    week07: (task) create GameItem data class

--- src/oop_00000110617_jesslynclarestasanders/week07/GameModels.kt
+++ src/oop_00000110617_jesslynclarestasanders/week07/GameModels.kt
@@ -3,3 +3,5 @@ package oop_00000110617_jesslynclarestasanders.week07
 enum class ItemRarity(val dropChance: Int) {
     COMMON(70), UNCOMMON(27), RARE(2), EPIC(3), LEGENDARY(1)
 }
+
+data class GameItem(val name: String, val damage: Int, val rarity: ItemRarity)
