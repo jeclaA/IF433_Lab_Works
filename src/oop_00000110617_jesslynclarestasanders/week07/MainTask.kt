81d18e9a970e0c7bb8dd9e142fbc3336b2913c1a
Jesslyn Claresta Sanders <jesslynclarestasanders@Jesslyns-MacBook-Pro.local>
2026-03-27 11:14:05 +0700

    week07: (task) test GameManager singleton in main

---
+++ src/oop_00000110617_jesslynclarestasanders/week07/MainTask.kt
@@ -0,0 +1,7 @@
+package oop_00000110617_jesslynclarestasanders.week07
+
+fun main() {
+    println("=== TEST GAME MANAGER (SINGLETON) ===")
+    GameManager.startGame()
+    GameManager.startGame()
+}
