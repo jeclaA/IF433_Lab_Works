219b2d2a344b288fc3dc5f49a3344457d57323db
Jesslyn Claresta Sanders <jesslynclarestasanders@Jesslyns-MacBook-Pro.local>
2026-04-17 11:07:03 +0700

    week08: create LegacyJavaAPI class in Java

---
+++ src/oop_00000110617_jesslynclarestasanders/week08/LegacyJavaAPI.java
@@ -0,0 +1,7 @@
+package oop_00000110617_jesslynclarestasanders.week08;
+
+public class LegacyJavaAPI {
+    public static String fetchServerStatus() {
+        return "SERVER_ONLINE_200_OK";
+    }
+}
