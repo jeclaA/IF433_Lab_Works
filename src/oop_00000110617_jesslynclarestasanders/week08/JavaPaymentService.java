ccc3213864a66a1260acab193769cfdb32e44f9d
Jesslyn Claresta Sanders <jesslynclarestasanders@Jesslyns-MacBook-Pro.local>
2026-04-17 11:20:13 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_00000110617_jesslynclarestasanders/week08/JavaPaymentService.java
@@ -0,0 +1,8 @@
+package oop_00000110617_jesslynclarestasanders.week08;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
+
