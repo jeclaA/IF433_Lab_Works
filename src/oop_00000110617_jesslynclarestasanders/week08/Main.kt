c20900a14e391d1bb867840656a79909bc636292
Jesslyn Claresta Sanders <jesslynclarestasanders@Jesslyns-MacBook-Pro.local>
2026-04-17 10:42:43 +0700

    week08: implement safe call with let block

--- src/oop_00000110617_jesslynclarestasanders/week08/Main.kt
+++ src/oop_00000110617_jesslynclarestasanders/week08/Main.kt
@@ -5,4 +5,12 @@ fun main() {
     val emptyOrder = Order(null, null)
     val destination = emptyOrder.deliveryDetails?.address?.city?.name ?: "Kota Tidak Diketahui"
     println("Tujuan Pengiriman: $destination")
+
+    println("\n=== TEST LET BLOCK ===")
+    val valiOrder = Order(null, 250000)
+    val receipt = valiOrder.totalPrice?.let { price ->
+        val tax = price * 0.11
+        "Transaksi Valid. Harga: Rp$price, Pajak: Rp$tax"
+    } ?: "Transaksi Invalid: Harga belum di-set!"
+    println(receipt)
 }
