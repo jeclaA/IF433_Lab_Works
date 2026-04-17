c81985e6d3271b9b5180f6e2d9f67639bbc6daa6
Jesslyn Claresta Sanders <jesslynclarestasanders@Jesslyns-MacBook-Pro.local>
2026-04-17 11:18:54 +0700

    week08: (task) implement robust ApiParser using Elvis and requireNotNull

---
+++ src/oop_00000110617_jesslynclarestasanders/week08/ApiParser.kt
@@ -0,0 +1,23 @@
+package oop_00000110617_jesslynclarestasanders.week08
+
+class ApiParser {
+    fun parseProduct(rawJson: Map<String, Any?>): Product? {
+        val id = requireNotNull(rawJson["id"] as? String) {
+            "API Invalid: Missing ID"
+        }
+        val name = requireNotNull(rawJson["name"] as? String) {
+            "API Invalid: Missing Name"
+        }
+        return when (rawJson["type"] as? String) {
+            "ELECTRONIC" -> {
+                val warrantyMonths = (rawJson["warranty"] as? Int) ?: 12
+                Product.Electronic(id, name, warrantyMonths)
+            }
+            "CLOTHING" -> {
+                val size = (rawJson["size"] as? String) ?: "All Size"
+                Product.Clothing(id, name, size)
+            }
+            else -> null
+        }
+    }
+}
