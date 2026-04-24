eb12b356cc5bbf30259a4b31f68dc4d94d6b8370
Jesslyn Claresta Sanders <jesslynclarestasanders@Jesslyns-MacBook-Pro.local>
2026-04-24 11:09:13 +0700

    week09: (task) test full crypto trading data pipeline

--- src/oop_00000110617_jesslynclarestasanders/week09/TradingDashboard.kt
+++ src/oop_00000110617_jesslynclarestasanders/week09/TradingDashboard.kt
@@ -2,12 +2,12 @@ package oop_00000110617_jesslynclarestasanders.week09
 
 fun main() {
     val tradeHistory = listOf(
-        TradeLog("BTCUSDT", "LONG", 10, 5.2, "CLOSED"),   // profit
-        TradeLog("ETHUSDT", "SHORT", 5, -2.3, "CLOSED"),  // loss
-        TradeLog("BTCUSDT", "LONG", 15, 3.8, "OPEN"),     // profit (belum close)
-        TradeLog("ETHUSDT", "LONG", 8, -1.5, "OPEN"),     // loss (belum close)
-        TradeLog("BTCUSDT", "SHORT", 20, 7.1, "CLOSED"),  // profit
-        TradeLog("ETHUSDT", "SHORT", 12, -4.0, "CLOSED")  // loss
+        TradeLog("BTCUSDT", "LONG", 10, 5.2, "CLOSED"),
+        TradeLog("ETHUSDT", "SHORT", 5, -2.3, "CLOSED"),
+        TradeLog("BTCUSDT", "LONG", 15, 3.8, "OPEN"),
+        TradeLog("ETHUSDT", "LONG", 8, -1.5, "OPEN"),
+        TradeLog("BTCUSDT", "SHORT", 20, 7.1, "CLOSED"),
+        TradeLog("ETHUSDT", "SHORT", 12, -4.0, "CLOSED")
     )
     val closedTrades = tradeHistory
         .filter { it.status == "CLOSED" }
