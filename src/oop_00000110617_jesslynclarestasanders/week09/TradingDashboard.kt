f19d3bd85faf86daf71e1fc4f22703cc5b52c3a9
Jesslyn Claresta Sanders <jesslynclarestasanders@Jesslyns-MacBook-Pro.local>
2026-04-24 11:04:55 +0700

    week09: (task) extract unique trading pairs using map and toSet

--- src/oop_00000110617_jesslynclarestasanders/week09/TradingDashboard.kt
+++ src/oop_00000110617_jesslynclarestasanders/week09/TradingDashboard.kt
@@ -25,4 +25,7 @@ fun main() {
         .map {
             "LOSS [${it.pair} - ${it.position}]: ${it.roe}% ROE (Lev: ${it.leverage}x)"
         }
+    val uniquePairs = tradeHistory
+        .map { it.pair }
+        .toSet()
 }
