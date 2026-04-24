e9ed0b577561a8387fe59cbc91ac5d14a22bf6da
Jesslyn Claresta Sanders <jesslynclarestasanders@Jesslyns-MacBook-Pro.local>
2026-04-24 11:05:57 +0700

    week09: (task) print top performers using forEach

--- src/oop_00000110617_jesslynclarestasanders/week09/TradingDashboard.kt
+++ src/oop_00000110617_jesslynclarestasanders/week09/TradingDashboard.kt
@@ -28,4 +28,8 @@ fun main() {
     val uniquePairs = tradeHistory
         .map { it.pair }
         .toSet()
+
+    println("\n=== CRYPTO TRADING DASHBOARD ===\n")
+    println("Top Performers:")
+    topPerformersString.forEach { println(it) }
 }
