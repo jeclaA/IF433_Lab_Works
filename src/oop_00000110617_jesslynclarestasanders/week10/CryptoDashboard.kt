9e8b2e015634b3fe6a451a4100f72082a4e07e83
Jesslyn Claresta Sanders <jesslynclarestasanders@Jesslyns-MacBook-Pro.local>
2026-05-07 12:14:05 +0700

    week10: (task) print dashboard using forEach on generic list

--- src/oop_00000110617_jesslynclarestasanders/week10/CryptoDashboard.kt
+++ src/oop_00000110617_jesslynclarestasanders/week10/CryptoDashboard.kt
@@ -9,4 +9,10 @@ fun main() {
 
     val response = ApiResponse("200 OK", coinRepo.getAll())
 
+    println("Status: ${response.status}")
+
+    response.data.forEach { coin ->
+        println("Coin: ${coin.name}, Balance: ${coin.balance}")
+    }
+
 }
