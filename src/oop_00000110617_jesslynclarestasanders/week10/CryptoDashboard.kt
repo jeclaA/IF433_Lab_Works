0b0bf84d0770808f493a951cbd91dcf97367f4b9
Jesslyn Claresta Sanders <jesslynclarestasanders@Jesslyns-MacBook-Pro.local>
2026-05-07 12:12:02 +0700

    week10: (task) populate coin repository

--- src/oop_00000110617_jesslynclarestasanders/week10/CryptoDashboard.kt
+++ src/oop_00000110617_jesslynclarestasanders/week10/CryptoDashboard.kt
@@ -2,4 +2,9 @@ package oop_00000110617_jesslynclarestasanders.week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+
+    coinRepo.add(Coin("BTC", 1.68))
+    coinRepo.add(Coin("ETH", 10.5))
+    coinRepo.add(Coin("USDT", 6700.0))
+
 }
