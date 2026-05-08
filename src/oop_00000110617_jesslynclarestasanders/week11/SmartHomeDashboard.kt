fa298bb9a026567efc3c5bf865998c0638a99b95
Jesslyn Claresta Sanders <jesslynclarestasanders@Jesslyns-MacBook-Pro.local>
2026-05-08 10:57:56 +0700

    week11: (task) configure Philips WiZ using apply

--- src/oop_00000110617_jesslynclarestasanders/week11/SmartHomeDashboard.kt
+++ src/oop_00000110617_jesslynclarestasanders/week11/SmartHomeDashboard.kt
@@ -2,5 +2,13 @@ package oop_00000110617_jesslynclarestasanders.week11
 
 fun main() {
     val homeDevices = mutableListOf<SmartDevice>()
+    val philipsLamp = SmartDevice("", "").apply {
+        name = "Philips WiZ Living Room"
+        category = "Lighting"
+        isOnline = true
+        powerLoad = 12
+    }.also {
+        homeDevices.add(it)
+    }
 }
 
