4c0cfd9b77d090646cce0484d422763282a49108
Jesslyn Claresta Sanders <jesslynclarestasanders@Jesslyns-MacBook-Pro.local>
2026-05-08 10:59:36 +0700

    week11: (task) configure AC unit using run

--- src/oop_00000110617_jesslynclarestasanders/week11/SmartHomeDashboard.kt
+++ src/oop_00000110617_jesslynclarestasanders/week11/SmartHomeDashboard.kt
@@ -20,5 +20,22 @@ fun main() {
         println("(LOG) Kamera terhubung")
         homeDevices.add(it)
     }
+    val airConditioner = run {
+        SmartDevice(
+            "Daikin Inverter (Kabel 3x2.5)",
+            "HVAC",
+            false,
+            800
+        )
+    }
+    homeDevices.add(airConditioner)
+    homeDevices.add(
+        SmartDevice(
+            "Picolo's Auto Feeder",
+            "Pet Care",
+            true,
+            10
+        )
+    )
 }
 
