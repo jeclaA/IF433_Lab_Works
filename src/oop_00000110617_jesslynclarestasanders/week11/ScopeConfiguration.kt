35cadee11d2bfa7a198d26db1e2c8b1d37c39283
Jesslyn Claresta Sanders <jesslynclarestasanders@Jesslyns-MacBook-Pro.local>
2026-05-08 10:30:55 +0700

    week11: combine apply and also in object setup

--- src/oop_00000110617_jesslynclarestasanders/week11/ScopeConfiguration.kt
+++ src/oop_00000110617_jesslynclarestasanders/week11/ScopeConfiguration.kt
@@ -19,4 +19,9 @@ fun main() {
     with(user) {
         println("User Detail -> Nama: $name, Umur: $age")
     }
+    val newUser = User("Budi", 20).apply {
+        age = 21
+    }.also {
+        println("User baru berhasil dibuat: $it")
+    }
 }
