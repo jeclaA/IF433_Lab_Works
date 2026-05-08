e7cca2fb5440af2d9778b589d4a2be744417ff93
Jesslyn Claresta Sanders <jesslynclarestasanders@Jesslyns-MacBook-Pro.local>
2026-05-08 10:20:03 +0700

    week11: implement let function for null safety

---
+++ src/oop_00000110617_jesslynclarestasanders/week11/ScopeLetRun.kt
@@ -0,0 +1,11 @@
+package oop_00000110617_jesslynclarestasanders.week11
+
+fun main(){
+    println("=== TEST LET FUNCTION ===")
+    val name: String? = "Alexander"
+    val length = name?.let {
+        println("Nama terdeteksi: $it")
+        it.length
+    }
+    println("Panjang nama: $length")
+}
