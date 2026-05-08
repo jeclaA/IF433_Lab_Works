1f2a568649480e33f4569e383a811fdbc9b260d8
Jesslyn Claresta Sanders <jesslynclarestasanders@Jesslyns-MacBook-Pro.local>
2026-05-08 10:26:41 +0700

    week11: implement apply for object initialization

--- src/oop_00000110617_jesslynclarestasanders/week11/ScopeConfiguration.kt
+++ src/oop_00000110617_jesslynclarestasanders/week11/ScopeConfiguration.kt
@@ -1,3 +1,12 @@
 package oop_00000110617_jesslynclarestasanders.week11
 
 data class User(var name: String = "", var age: Int = 0)
+
+fun main() {
+    println("=== TEST APPLY ===")
+    val user = User().apply {
+        name = "Alex"
+        age = 35
+    }
+    println(user)
+}
